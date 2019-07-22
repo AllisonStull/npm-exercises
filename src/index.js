59c94a8a3c3eb185d984baf09a00763d14ca29a9
Allison Stull <allisonstull3@gmail.com>
2019-07-22 08:29:56 -0500

    modules exercise

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+// messing around
+// const sayHello = () => console.log("hello")
+// sayHello();
+//
+// const $ = require('jquery');
+// $("body").css("background-color", "red");
+// const hello = require('./say-hello.js');
+// //
+// // console.log(hello());
+//
+
+
+// JS 2 modules exercise
+//1
+// import {hello} from "./say-hello.js"
+// console.logt(hello())
+
+//2
+import message from "./say-hello.js"
+console.log(message.hello());
---
+++ src/say-hello.js
@@ -0,0 +1,12 @@
+// JS 2 Modules exercise
+//1
+
+// export const hello = () => {
+//     return "hi"
+// };
+
+//2
+const hello = () => {
+    return "hi"
+};
+export default {hello}
